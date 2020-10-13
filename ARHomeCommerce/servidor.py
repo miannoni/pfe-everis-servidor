e93da5abeea60fe2d3b6dc478d4ea29f4b355098
Your Name <matteoi@al.insper.edu.br.com>
2020-10-13 13:04:09 -0300

    update servidor melhor

--- ARHomeCommerce/servidor.py
+++ ARHomeCommerce/servidor.py
@@ -3,17 +3,59 @@ from typing import Optional
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+class Produto(BaseModel):
+    _id: int
+
+class Pedido(BaseModel):
+    _id: int
+    data: str
+    produto: Produto
+    detalhes: Optional[str] = None
+    entregue: Optional[int] = 0
+
+class Cartao(BaseModel):
+    numero: str
+    dono: str
+    validade: str
+    codigo: str
+
 class User(BaseModel):
     username: str
     password: str
     bio: Optional[str] = None
+    pedidos: Optional[dict] = None
+    cartoes: Optional[dict] = None
+    enderecos: Optional[dict] = None
 
 app = FastAPI()
 
-users = {}
+produtos = {
+                1 : { 
+                    "nome" : "Cocacola",
+                    "preco" : "2,99"
+                    },
+                2 : { 
+                    "nome" : "Pepsi",
+                    "preco" : "2,29"
+                    }
+            }
+
+users = { 
+            "Matt" : {
+                    "username" : "Matt",
+                    "password" : "12345",
+                    "bio" : "Hello World"
+                    }
+        }
+
+@app.get("/")
+async def root():
+    return 404
+
+################### LOGIN ###################
 
 @app.get("/login")
-async def root():
+async def login():
     return users
 
 @app.post("/login", response_description="Data added into the database")
@@ -39,3 +81,51 @@ async def del_student_data(user: User):
         return user.username + " has been deleted"
     else:
         return "User " + user.username + " does not exist in database"
+
+################### CARTAO ###################
+
+@app.get("/login/cartao")
+async def cartoes():
+    if (user.username in users.keys()):
+        if (user.password == users[user.username]["password"]):
+            return users[user.username]["cartoes"]
+
+@app.post("/login/cartao", response_description="Data added into the database")
+async def create_user_data(user: User):
+    if (user.username in users.keys()):
+        if (user.password == users[user.username]["password"]):
+            users[user.username]["cartoes"][] = user.cartoes
+
+# @app.put("/login/cartao", response_description="Data modified in the database")
+# async def mod_student_data(user: User):
+    # if (user.username in users.keys()):
+    #     users[user.username] = user
+    #     return user.username + " has been modified"
+    # else:
+    #     return "User " + user.username + " does not exist in database"
+
+# @app.delete("/login/cartao", response_description="Data modified in the database")
+# async def del_student_data(user: User):
+
+#     if (user.username in users.keys()):
+#         del users[user.username]
+#         return user.username + " has been deleted"
+#     else:
+#         return "User " + user.username + " does not exist in database"
+
+################### ENDERECO ###################
+@app.get("/login/endereco")
+async def cartoes():
+    if (user.username in users.keys()):
+        if (user.password == users[user.username]["password"]):
+            return users[user.username]["cartoes"]
+
+@app.post("/login/endereco", response_description="Data added into the database")
+async def create_user_data(user: User):
+    if (user.username in users.keys()):
+        if (user.password == users[user.username]["password"]):
+            users[user.username]["cartoes"] = user.cartoes
+
+################### PEDIDO ###################
+
+################### PRODUTO ###################
--- Procfile
+++ Procfile
@@ -1,1 +1,5 @@
-web: uvicorn ARHomeCommerce.servidor:app --host=0.0.0.0 --port=${PORT:-80}
+sudo apt update
+sudo apt upgrade -Y
+sudo apt install python3-pip
+sudo pip3 install -r requirements.txt
+sudo uvicorn ARHomeCommerce.servidor:app --host=0.0.0.0 --port=${PORT:-80}
